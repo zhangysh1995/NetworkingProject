9075042af4bf4b50679355b7af73356c6504f44b
zhangysh1995 <zhangysh1995@gmail.com>
2017-12-27 19:26:17 +0800

    mail services; update

--- SecuredIM/Utility/GPG.java
+++ SecuredIM/Utility/GPG.java
@@ -1,20 +1,49 @@
 package Utility;
 
+import java.io.IOException;
+
 /**
  * This module deals with encryption & decryption
  */
 
 //import bounty
 public class GPG {
+    private String privateKey;
+    private String publicKey;
+    private String email;
+
     public GPG() {
 
     }
 
-    private String Decryption() {
-        return "";
+    public Boolean newKey(String email) {
+        // get new keys
+        if(email != null) this.email = email;
+        return true;
     }
 
-    private String Encryption() {
-        return "";
+    public static String Decrypt(String text) {
+        return "Decrypted text";
+    }
+
+    public static String Encrypt(String text) {
+        return "Encrypted text";
+    }
+
+    // getter & setters
+    public String getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(String privateKey) {
+        this.privateKey = privateKey;
+    }
+
+    public String getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(String publicKey) {
+        this.publicKey = publicKey;
     }
 }
--- SecuredIM/Utility/MailHandler.java
+++ SecuredIM/Utility/MailHandler.java
@@ -4,27 +4,99 @@ package Utility;
  * this module handles mail services
  */
 
-public class MailHandler {
-    private static String fromMail;
+import Utility.GPG;
+import java.util.Date;
+import java.util.Dictionary;
+import java.util.Properties;
 
-    public static void setMail(String mail) {
-        fromMail = mail;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class MailHandler {
+    private static String myMail;
+    private String mySmtpServer;
+
+    private String request = "newFriendRequest";
+//    private smtpServerList;
+
+    private Properties props;
+
+    public MailHandler() {
+        this.props = System.getProperties();;
     }
 
-    public static String getMail() {
-        try {
-            if (fromMail != null)
-                return fromMail;
-//            else throw  NullPointerException;
+    public static void setMail(String mail) {
+        myMail = mail;
+    }
+
+    private void setMySmtpServert() {
+
+    }
+
+    public static String getMail() throws NullPointerException{
+        String mail = "";
+
+        try
+        {
+            if (myMail != null)
+                mail = myMail;
+            else {
+                mail = null;
+                throw new NullPointerException("Your email was not set!!!");
+            }
         } catch (NullPointerException e) {
             e.printStackTrace();
         }
-    }
-    public Boolean sendRequestMail(String email) {
-        return true;
+
+        return mail;
     }
 
-    public Boolean sendMessage() {
+    public Boolean sendRequestMail(String toEmail) {
+        return sendMessage(toEmail, request);
+    }
+
+    public Boolean sendMessage(String toEmail, String text ) {
+        text = GPG.Encrypt(text);
+        return send(toEmail, text);
+    }
+
+    private Boolean send(String toEmail, String text) {
+        final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
+
+        props.setProperty("mail.smtp.host","smtp.mxhichina.com");
+        props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
+        props.setProperty("mail.smtp.socketFactory.fallback", "false");
+        props.setProperty("mail.smtp.port", "465");
+        props.setProperty("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.auth", "true");
+
+        final String username="zhangyushao@zhangyushao.site";
+        final String password="Kid1412kelly";
+        Session session =  Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
+        // set message
+        Message msg = new MimeMessage(session);
+        try
+        {
+        msg.setFrom(new InternetAddress(myMail));
+        msg.setRecipients(Message.RecipientType.TO,
+                InternetAddress.parse(toEmail, false));
+        msg.setSubject("CYY/1.0\r\n");
+
+        msg.setText(text);
+        msg.setSentDate(new Date());
+
+        Transport.send(msg);
+        } catch(MessagingException e) {
+            System.err.println("Error when constructing the message");
+        }
+
         return true;
     }
 
@@ -34,5 +106,9 @@ public class MailHandler {
 
     public Boolean receiveMessage() {
         return true;
+    }
+
+    private String receive() {
+        return "";
     }
 }
