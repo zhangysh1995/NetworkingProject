6dbeda3ddb87779532ff5ad3330fa0aee50fd6db
zhangysh1995 <zhangysh1995@gmail.com>
2017-12-30 17:39:12 +0800

    link button to code

--- SecuredIM/IM_GUI/Chatting/P2PchatController.java
+++ SecuredIM/IM_GUI/Chatting/P2PchatController.java
@@ -1,10 +1,16 @@
 package IM_GUI.Chatting;
 
+import Utility.MailHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 public class P2PchatController {
     private MouseEvent mouseEvent;
@@ -13,10 +19,15 @@ public class P2PchatController {
     private String email;
     private int index;
 
+    private MailHandler mailHandler;
+
     @FXML private Label title;
+    @FXML private ScrollPane scrollPane;
     @FXML private TextField userInput;
+    @FXML private TextFlow textLog;
 
     public P2PchatController(MouseEvent mouseEvent) {
+        mailHandler = new MailHandler();
         this.mouseEvent = mouseEvent;
         this.listView = (ListView) mouseEvent.getSource();
         email = (String) listView.getSelectionModel().getSelectedItem();
@@ -24,8 +35,28 @@ public class P2PchatController {
     }
 
     @FXML
+    public void sendMessage(KeyEvent keyEvent) {
+        if(keyEvent.getCode() == KeyCode.ENTER) {
+            String input = ":\n" + userInput.getText() + "\n";
+            Text msg = new Text(MailHandler.getMail() + input);
+//            mailHandler.sendMessage(email, userInput.getText());
+            showNewMessage(msg);
+        }
+    }
+
+    @FXML
+    public void ReceiveMessage(){};
+
+    @FXML
     public void initialize() {
         title.setText("Chatroom: " + email);
+        textLog.setLineSpacing(5);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         userInput.setText("Enter here...press `Enter` to send");
+    }
+
+    private void showNewMessage(Text msg) {
+        textLog.getChildren().add(msg);
+        scrollPane.setVvalue(scrollPane.getHmax());
     }
 }
